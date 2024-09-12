e4d31c76d6f88b88d3def731e2b418f3d6b72e48
suhailabdaz <suhail.abdaz@gmail.com>
2024-09-12 12:24:34 +0530

    express

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import { connectDB } from "./config/mongoDB.config";
 import RabbitMQClient from "./rabbitMQ/client"
+import express from "express";
+import { Application } from "express";
+import "dotenv/config";
+
+
 
 
 
 class App{
+  public app: Application;
+
   constructor(){
+    this.app=express()
+    this.app.listen(process.env.USER_PORT,()=>{
+      console.log(`server  http://localhost:${process.env.USER_PORT}`);
+      
+    })
       connectDB()
       RabbitMQClient.initialize()
   }
 }
 
 export default App
+
+
